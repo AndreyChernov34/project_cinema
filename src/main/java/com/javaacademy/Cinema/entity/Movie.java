7b42778638d687e141e41121be10dc4b1573f537
AndreyChernov34 <andrey.chernov34@gmail.com>
2025-02-11 01:30:09 +0300

    2 этап закончен

---
+++ src/main/java/com/javaacademy/Cinema/entity/Movie.java
@@ -0,0 +1,13 @@
+package com.javaacademy.Cinema.entity;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private Integer id;
+    private String name;
+    private String description;
+
+
+
+}
---
+++ src/main/java/com/javaacademy/Cinema/entity/Place.java
@@ -0,0 +1,9 @@
+package com.javaacademy.Cinema.entity;
+
+import lombok.Data;
+
+@Data
+public class Place {
+    private Integer id;
+    private String name;
+}
